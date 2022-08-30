a80834ec013fab81bdb4bd20f6e1103b2f34c909
Yuta0227 <yutahonjo@keio.jp>
2022-08-31 03:33:30 +0900

    auth::checkの判定逆になってた

--- src/resources/views/header.blade.php
+++ src/resources/views/header.blade.php
@@ -1,7 +1,7 @@
 <header class="flex top-0 left-0 h-40 w-screen items-center" style="padding:10px;background-color:#EEF6FF;">
     <div class="font-bold w-1/2" style="width:50%;font-weight:bold;">パズ村</div>
     <div class="w-2/5 flex justify-end items-center" style="width:40%;">
-        @if (Auth::check())
+        @if (!Auth::check())
             <div class="w-1/2 bg-slate-50">
                 <a href="{{ route('login') }}">
                     <img src="{{ asset('img/Frame8.svg') }}">
