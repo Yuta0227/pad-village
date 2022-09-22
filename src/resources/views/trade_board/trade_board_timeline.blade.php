e7f2b7212b7782dee3431d4547348934c59d337f
Yuta0227 <yutahonjo@keio.jp>
2022-09-23 01:01:45 +0900

    フロント任せるために一旦push

--- src/resources/views/trade_board/trade_board_timeline.blade.php
+++ src/resources/views/trade_board/trade_board_timeline.blade.php
@@ -8,9 +8,11 @@
     <div class="h-8" id="title_timeline">
         <h2 class="h-8 ml-5 font-bold fixed w-full bg-blue-50">タイムライン</h2>
     </div>
-    <section id="post_trade_form_section" style="z-index:100;height:100vh;overflow-y:scroll;position:fixed;overscroll-behavior-y:none;" class="hidden bg-blue-50 w-full">
+    <section id="post_trade_form_section"
+        style="z-index:100;height:100vh;overflow-y:scroll;position:fixed;overscroll-behavior-y:none;"
+        class="hidden bg-blue-50 w-full">
         <button id="close_form">
-            <img src="{{ asset('img/close_modal.svg')}}" alt="cross" width="28">
+            <img src="{{ asset('img/close_modal.svg') }}" alt="cross" width="28">
         </button>
         <form action="/boards/trade" method="POST">
             @csrf
@@ -89,9 +91,11 @@
                 </div>
                 <div id="increase_monster_gives">出を増やす</div>
             </div>
-            <textarea name="description" rows="8">@if (session()->has('description'))
+            <textarea name="description" rows="8">
+@if (session()->has('description'))
                 {{ session()->get('description') }}
-                @endif</textarea>
+                @endif
+</textarea>
             <label>
                 <input type="checkbox" name="allow_show_pad_id_bool"
                     @if (session()->has('allow_show_pad_id_bool')) @if (session()->get('allow_show_pad_id_bool') === 'on')
@@ -164,15 +168,37 @@
                 <div class="delete_give_box" onclick="delete_give_box(${next_give_box_id_only_number+1})">消す</div>
                 </div>`);
         });
+
+        function disableScroll(event) {
+            event.preventDefault();
+        }
         document.getElementById('open_post_trade_form').addEventListener('click', function() {
             document.getElementById('title_timeline').classList.add('hidden');
             document.getElementById('post_trade_form_section').classList.remove('hidden');
             document.getElementById('open_post_trade_form').classList.add('hidden');
+            document.getElementById('all_posts').style.overflow = 'hidden';
         });
         document.getElementById('close_form').addEventListener('click', function() {
             document.getElementById('title_timeline').classList.remove('hidden');
             document.getElementById('post_trade_form_section').classList.add('hidden');
             document.getElementById('open_post_trade_form').classList.remove('hidden');
+            document.getElementById('all_posts').style.overflow = 'auto';
+        });
+        document.addEventListener('scroll', function() {
+            //     if(document.getElementById('all_posts').style.overflow=='hidden'){
+            //         console.log('ok');
+            //         document.addEventListener('touchmove',disableScroll,{passive:false})
+            //     }
+
+            if (document.getElementById('all_posts').style.overflow == 'hidden') {
+                document.addEventListener('touchmove', disableScroll, {
+                    passive: false
+                });
+                document.addEventListener('mousewheel', disableScroll, {
+                    passive: false
+                });
+                // document.getElementById('all_posts').style.overflowY='hidden';
+            }
         });
     </script>
 @endsection
