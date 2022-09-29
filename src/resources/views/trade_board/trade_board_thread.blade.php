6782645fac162df03a2bbcbcaa3f134894416eed
ryudai <bigdragon.06102001@gmail.com>
2022-09-30 00:10:25 +0900

    親投稿と返信のボーダーの色を変更

--- src/resources/views/trade_board/trade_board_thread.blade.php
+++ src/resources/views/trade_board/trade_board_thread.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.user_page')
 @section('content')
-<div class="h-8">
-    <h2 class="flex ml-5 font-bold fixed w-full bg-blue-50">
-        <a @if (empty($post->parent_trade_board_post_id)) href="/boards/trade"
+    <div class="h-8">
+        <h2 class="flex ml-5 font-bold fixed w-full bg-blue-50">
+            <a @if (empty($post->parent_trade_board_post_id)) href="/boards/trade"
             @else
             href="/boards/trade/{{ $post->parent_trade_board_post_id }}" @endif
-            class="block mr-2 my-auto"><img src="{{ asset('/img/go_back_arrow.svg') }}" class="-mt-1"></a>
+                class="block mr-2 my-auto"><img src="{{ asset('/img/go_back_arrow.svg') }}" class="-mt-1"></a>
             <span>スレッド</span>
         </h2>
     </div>
     <section class="px-5 pb-8">
         <x-trade-post-card :post="$post" />
-        <section class="flex flex-col gap-5 mt-5 pt-5 border-t-2">
+        <section class="flex flex-col gap-5 mt-5 pt-5 border-t-2 border-gray-400">
             @foreach ($replies as $reply)
                 <x-trade-post-card :post="$reply"></x-trade-post-card>
             @endforeach
@@ -23,5 +23,5 @@
     </button>
     <form id="" hidden action="/boards/trade/create">
     </form>
-</div>
+    </div>
 @endsection
